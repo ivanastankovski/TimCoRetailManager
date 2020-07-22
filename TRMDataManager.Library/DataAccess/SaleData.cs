06563b1e2e26ffe8d295c902c6cd474973901555
Ivana S <ivanastankovski91@gmail.com>
2020-07-23 01:02:53 +0200

    Add Saving the Cart to the Database
    All the changes necessary to save the shopping cart data to the db.

---
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDataManager.Library.Internal.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class SaleData
+    {
+        public void SaveSale(SaleModel saleInfo, string cashierId)
+        {
+            // TODO: Make this SOLID/DRY/Better
+            // Start filling in the Sale Detail Models we will save to the database
+            // Fill in the available information
+            List<SaleDetailDBModel> details = new List<SaleDetailDBModel>();
+            ProductData products = new ProductData();
+            var taxRate = ConfigHelper.GetTaxRate()/100;
+
+            foreach (var item in saleInfo.SaleDetails)
+            {
+                var detail = new SaleDetailDBModel
+                {
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity
+                };
+
+                // Get the information about this product
+                var productInfo = products.GetProductById(detail.ProductId);
+
+                if (productInfo == null)
+                {
+                    throw new Exception($"The product Id of { detail.ProductId } could not be found in the database.");
+                }
+
+                detail.PurchasePrice = (productInfo.RetailPrice * detail.Quantity);
+
+                if (productInfo.IsTaxable)
+                {
+                    detail.Tax = (detail.PurchasePrice * taxRate);
+                }
+
+                details.Add(detail);
+            }
+
+            // Create the Sale Model
+            SaleDBModel sale = new SaleDBModel
+            {
+                SubTotal = details.Sum(x => x.PurchasePrice),
+                Tax = details.Sum(x => x.Tax),
+                CashierId = cashierId
+            };
+
+            sale.Total = sale.SubTotal + sale.Tax;
+
+            // Save the Sale Model once we populate it
+            SqlDataAccess sql = new SqlDataAccess();
+            sql.SaveData<SaleDBModel>("dbo.spSale_Insert", sale, "TRMData");
+
+            // Get the ID from the Sale Model
+            sale.Id = sql.LoadData<int, dynamic>("spSaleLookup", new { sale.CashierId, sale.SaleDate}, "TRMData").FirstOrDefault();
+
+            // Finish filling in the sale detail models
+            foreach (var item in details)
+            {
+                item.SaleId = sale.Id;
+                // Save the sale detail models
+                sql.SaveData("dbo.spSaleDetail_Insert", item, "TRMData");
+            }
+        }
+    }
+}
